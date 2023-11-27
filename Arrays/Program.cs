6c4e5bcf526c335a3e2b1b167331248bf899fecf
nikocukaroslav <nikocukaroslav2006@gmail.com>
2023-11-27 20:22:42 +0200

    Виконано завдань 6/6

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,21 +1,57 @@
 ﻿int n;
+bool acces1 = false, acces2 = false;
 Console.WriteLine("Задайте кiлькiсть дiйсних чисел массиву:");
+do { 
 if (int.TryParse(Console.ReadLine(), out n))
 {
-    double sum = 0;
-       Random rand = new Random();
-    double[] Array = new double[n];
-    double arrayMin = Array[0];
-    Console.WriteLine("Згенерований массив:");
-    for (int i = 0; i < n; i++)
+    acces1 = true;
+    int a, b;
+    Console.WriteLine("Задайте проміжок генерації дійсних чисел");
+    do
     {
-        Array[i] = rand.NextDouble()*10-5;
-        Array[i] = Math.Round(Array[i], 2);
-        if (Array[i] < 0) sum += Array[i];
-        if (arrayMin > Array[i]) arrayMin= Array[i];
-        Console.Write($"{Array[i]} ");
-    }
-    Console.WriteLine();
-    Console.WriteLine($"Сума вiд'ємних елементiв массиву = {sum}");
-    Console.WriteLine($"Мінімальний елемент массиву = {arrayMin}");
+        if (int.TryParse(Console.ReadLine(), out a) && int.TryParse(Console.ReadLine(), out b))
+        {
+            acces2 = true;
+            Random rand = new Random();
+            double[] Array = new double[n];
+            double arrayMin = double.MaxValue;
+            double sum = 0;
+            double Max = double.MinValue;
+            int maxIndex = 0;
+            double abs = double.MinValue;
+            double absMax = 0;
+            int indexSum = 0;
+            int intCount = 0;
+            Console.WriteLine("Згенерований массив:");
+            for (int i = 0; i < n; i++)
+            {
+                Array[i] = Math.Round(a + rand.NextDouble() * (b - a), 2);
+                if (Array[i] < 0) sum += Array[i];
+                if (arrayMin > Array[i]) arrayMin = Array[i];
+                if (Max < Array[i])
+                {
+                    Max = Array[i];
+                    maxIndex = i;
+                }
+                if (abs < Math.Abs(Array[i]))
+                {
+                    abs = Math.Abs(Array[i]);
+                    absMax = Array[i];
+                }
+                if (Array[i] > 0) indexSum += i;
+                if (Array[i] == (double)Math.Round(Array[i])) intCount++;
+                Console.Write($"{Array[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Сума вiд'ємних елементiв массиву = {sum}");
+            Console.WriteLine($"Мiнiмальний елемент массиву: {arrayMin}");
+            Console.WriteLine($"Iндекс максимального елементу массиву: {maxIndex}");
+            Console.WriteLine($"Максимальний за модулем елемент массиву: {absMax}");
+            Console.WriteLine($"Cума iндексiв додатнiх елементiв: {indexSum}");
+            Console.WriteLine($"Кiлькiсть цiлих чисел у масивi: {intCount}");
+        }
+        else Console.WriteLine("Задане невірне значення!");
+    } while (!acces2);
 }
+else Console.WriteLine("Задане невірне значення!");
+} while (!acces1) ;
