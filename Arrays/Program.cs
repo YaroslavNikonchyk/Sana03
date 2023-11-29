1d590202a25cb2e31b95cf88050961f6a87b508a
nikocukaroslav <nikocukaroslav2006@gmail.com>
2023-11-29 13:01:01 +0200

    Розбито завдання на методи, і оформлено окреме консольне рішення

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,4 +1,6 @@
-﻿int n;
+﻿using Arrays;
+
+int n;
 bool acces1 = false, acces2 = false;
 Console.WriteLine("Задайте кiлькiсть дiйсних чисел массиву:");
 do
@@ -13,43 +15,23 @@ do
             if (double.TryParse(Console.ReadLine(), out a) && double.TryParse(Console.ReadLine(), out b) && a < b)
             {
                 acces2 = true;
-                Random rand = new Random();
-                double[] Array = new double[n];
-                double arrayMin = double.MaxValue;
-                double sum = 0;
-                double Max = double.MinValue;
-                int maxIndex = 0;
-                double abs = double.MinValue;
-                double absMax = 0;
-                int indexSum = 0;
-                int intCount = 0;
-                Console.WriteLine("Згенерований массив:");
-                for (int i = 0; i < n; i++)
-                {
-                    Array[i] = Math.Round(a + rand.NextDouble() * (b - a), 2);
-                    if (Array[i] < 0) sum += Array[i];
-                    if (arrayMin > Array[i]) arrayMin = Array[i];
-                    if (Max < Array[i])
-                    {
-                        Max = Array[i];
-                        maxIndex = i;
-                    }
-                    if (abs < Math.Abs(Array[i]))
-                    {
-                        abs = Math.Abs(Array[i]);
-                        absMax = Array[i];
-                    }
-                    if (Array[i] > 0) indexSum += i;
-                    if (Array[i] == (double)Math.Round(Array[i])) intCount++;
-                    Console.Write($"{Array[i]} ");
-                }
+                double[] array = arrays.GenerateArray(n,a,b);
+                double negativeSum = arrays.SumOfNegativeElements(array);
+                double arrayMin = arrays.ArrayMinElement(array);
+                double indexMax = arrays.MaxElementIndex(array);
+                double absMax = arrays.AbsMaxElement(array);
+                double integersSum = arrays.IndexSumOfPositiveElements(array);
+                double integersCount = arrays.CountOfIntegers(array);
+                Console.WriteLine($"Массив псевдозгенерованих чисел:");
+                for (int i = 0; i < array.Length; i++)
+                    Console.Write($"{array[i]} ");
                 Console.WriteLine();
-                Console.WriteLine($"Сума вiд'ємних елементiв массиву = {sum}");
+                Console.WriteLine($"Сума вiд'ємних елементiв массиву = {negativeSum}");
                 Console.WriteLine($"Мiнiмальний елемент массиву: {arrayMin}");
-                Console.WriteLine($"Iндекс максимального елементу массиву: {maxIndex}");
+                Console.WriteLine($"Iндекс максимального елементу массиву: {indexMax}");
                 Console.WriteLine($"Максимальний за модулем елемент массиву: {absMax}");
-                Console.WriteLine($"Cума iндексiв додатнiх елементiв: {indexSum}");
-                Console.WriteLine($"Кiлькiсть цiлих чисел у масивi: {intCount}");
+                Console.WriteLine($"Cума iндексiв додатнiх елементiв: {integersSum}");
+                Console.WriteLine($"Кiлькiсть цiлих чисел у масивi: {integersCount}");
             }
             else Console.WriteLine("Задано невiрне значення!");
         } while (!acces2);
---
+++ Arrays/arrays.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class arrays
+    {
+        public static double[] GenerateArray(int elementsCount, double minValue = -10, double maxValue = 10, int digitCount = 2)
+        {
+            double[] array = new double[elementsCount];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = Math.Round(minValue + (random.NextDouble() * (maxValue - minValue)), digitCount);
+            return array;
+        }
+        public static double SumOfNegativeElements(double[] array)
+        {
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] < 0) sum += array[i];
+            return sum;
+        }
+        public static double ArrayMinElement(double[] array)
+        {
+            double min = double.MaxValue;
+            for(int i = 0;i < array.Length;i++) 
+            if (min > array[i]) min = array[i];
+            return min;
+        }
+        public static double MaxElementIndex(double[] array)
+        {
+            double max = array[0];
+            int maxIndex = 0;
+            for(int i=0; i < array.Length; i++)
+            if (max < array[i])
+            {
+                max = array[i];
+                maxIndex = i;
+            }
+            return maxIndex;
+        }
+        public static double AbsMaxElement(double[] array)
+        {
+            double abs = double.MinValue;
+            double absMax = 0;
+            for (int i =0;i< array.Length;i++)
+            if (abs < Math.Abs(array[i]))
+            {
+                abs = Math.Abs(array[i]);
+                absMax = array[i];
+            }
+            return absMax;
+        }
+        public static double IndexSumOfPositiveElements(double[] array)
+        {
+            int indexSum=0;
+            for (int i=0;i< array.Length;i++)   
+            if (array[i] > 0) indexSum += i;
+            return indexSum;
+        }
+        public static double CountOfIntegers(double[] array)
+        {
+            int count=0;
+            for(int i=0;i< array.Length;i++)
+            if (array[i] == (double)Math.Round(array[i])) count++;
+            return count;
+        }
+    }
+}
